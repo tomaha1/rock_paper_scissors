b48adb11c1cc3d69ffdad1facaa414e3cfb59da7
tomaha1 <tomaha1@gazeta.pl>
2022-12-22 19:12:45 +0100

    Add playRound function, correct getPlayerChoice function

--- javascript.js
+++ javascript.js
@@ -14,17 +14,39 @@ function getComputerChoice() {
 };
 
 function getPlayerChoice() {
-    let selectionLowerCase;
-    while (selectionLowerCase === undefined) {
-    let selection = prompt("Choose rock, paper or scissors: ");
-    let selectionLowerCase = selection.toLowerCase();
-    if (selectionLowerCase === "rock" || selectionLowerCase === "paper" || selectionLowerCase === "scissors") {
-        return selectionLowerCase;
-        } else {
+    let selection;
+    while (selection === undefined) {
+        let selection = prompt("Choose rock, paper or scissors: ").toLowerCase();
+        if (selection === "rock") {//||  || (selectionLowerCase === "scissors")) {
+            return selectionLowerCase = "rock";
+        } else if (selection === "paper") {
+            return selectionLowerCase = "paper";
+        } else if (selection === "scissors") {
+            return selectionLowerCase = "scissors";
+        }   else {
         console.log("Wrong word, try again.");
         }
     }
 };
 
+function playRound(user, computer) {
+    if (user === computer) {
+        return "It is a tie!";
+    }   else if (user === "paper" && computer === "rock") {
+        return "You won. Paper beats rock!";
+    }   else if (user === "rock" && computer === "scissors") {
+        return "You won. Rock beats scissors!";
+    }   else if (user === "scissors" && computer === "paper") {
+        return "You won. Scissors beat paper!"
+    }   else if (user === "scissors" && computer === "rock") {
+        return "You lost. Rock beats scissors!";
+    }   else if (user === "rock" && computer === "paper") {
+        return "You lost. Paper beats rock!";
+    }   else if (user === "paper" && computer === "scissors") {
+        return "You lost. Scissors beat paper!"
+    }
+}
+
 console.log(getPlayerChoice());
 console.log(getComputerChoice());
+console.log(playRound(selectionLowerCase, computerSelection));
