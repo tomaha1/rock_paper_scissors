82b7d4eb629cc8fc8b0c491d51fabbc0aa4d56c0
tomaha1 <tomaha1@gazeta.pl>
2022-12-25 10:31:53 +0100

    Add function game and modify function playRound

--- javascript.js
+++ javascript.js
@@ -33,20 +33,42 @@ function playRound(user, computer) {
     if (user === computer) {
         return "It is a tie!";
     }   else if (user === "paper" && computer === "rock") {
+        user_score +=1;
         return "You won. Paper beats rock!";
     }   else if (user === "rock" && computer === "scissors") {
+        user_score +=1;
         return "You won. Rock beats scissors!";
     }   else if (user === "scissors" && computer === "paper") {
-        return "You won. Scissors beat paper!"
+        user_score +=1;
+        return "You won. Scissors beat paper!";
     }   else if (user === "scissors" && computer === "rock") {
+        comp_score += 1;
         return "You lost. Rock beats scissors!";
     }   else if (user === "rock" && computer === "paper") {
+        comp_score += 1;
         return "You lost. Paper beats rock!";
     }   else if (user === "paper" && computer === "scissors") {
-        return "You lost. Scissors beat paper!"
+        comp_score += 1;
+        return "You lost. Scissors beat paper!";
     }
-}
+};
 
-console.log(getPlayerChoice());
-console.log(getComputerChoice());
-console.log(playRound(selectionLowerCase, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        getPlayerChoice();
+        getComputerChoice();
+        console.log(playRound(selectionLowerCase, computerSelection));
+        console.log(`Round ${i+1} results user: ${user_score} computer: ${comp_score}.`);
+    }
+    if (comp_score > user_score) {
+        console.log(`Computer won ${comp_score} to ${user_score}.`);
+    } else if (comp_score < user_score) {
+        console.log(`You won ${user_score} to ${comp_score}.`)
+    } else {
+        console.log("It is a tie!");
+    }
+};
+
+let user_score = 0;
+let comp_score = 0;
+game();
